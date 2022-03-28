0ec8d289553e7bb3ac1c1a235f0f4f23333becb7
calderad4081 <102534939+calderad4081@users.noreply.github.com>
2022-03-28 11:00:19 -0400

    created the menu

--- simplemenu/main.cpp
+++ simplemenu/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
 using namespace std;
+//Print menu
+// Get Input
+//If valid, do a thing
+//If not valid, repeat
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int userChoice = 0;
+
+    cout << "simple menu" << endl;
+    cout << "1. Option 1" << endl;
+    cout << "2. Exit" << endl;
+    cout << "Choose: ";
+    // cout << "You picked " << userChoice << endl;
+    // Take action based on menu
+    if (userChoice == 1) {
+        cout << "You picked Option 1." << endl;
+    }
+    else if (userChoice ==2) {
+        cout << "Exiting...goodbye." << endl;
+    }
+    else {
+        // not any of the above options
+        cout << "Invalid input, please try again." << endl;
+    }
     return 0;
 }
